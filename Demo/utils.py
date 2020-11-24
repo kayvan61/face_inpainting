909f2976f45848beaf8d530e6cd9e5db6b58cc66
kayvan61 <kayvan.mansoor@gmail.com>
2020-11-24 17:18:37 -0600

    added prototypes for functions

--- Demo/utils.py
+++ Demo/utils.py
@@ -1,20 +1,19 @@
-
 # input is an image
 # output is a list of the contours found in the image's edgemap
-def getContours():
+def getContours(image, method):
     pass
 
 # input is an image. This uses getContours and fillContours
 # returns a list of masks
-def getMasks():
+def getMasks(image):
     pass
     
 # input is an image. 
 # returns number of faces detected
-def countFaces():
+def countFaces(image):
     pass
     
 # input is a list of contours and image dimensions
 # returns a binary image of 1 with 0 on the inside of the contour.
-def fillContours():
+def fillContours(contours, width, height):
     pass
